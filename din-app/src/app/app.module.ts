0ff673718d7afc73274c1374d90651bf5a824e3d
pontep <pontep.taveeshup@gmail.com>
2020-11-12 15:17:37 +0700

    doing app layout

--- din-app/src/app/app.module.ts
+++ din-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './modules/index/index.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
