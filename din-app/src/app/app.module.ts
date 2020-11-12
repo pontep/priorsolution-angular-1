4b241b6df8a4d65973cbacdcff02bb90ff3d7188
pontep <pontep.taveeshup@gmail.com>
2020-11-12 12:06:38 +0700

    reset vscode

--- din-app/src/app/app.module.ts
+++ din-app/src/app/app.module.ts
@@ -8,7 +8,14 @@ import { Page2Component } from './modules/page2/page2.component';
 import { AppHeaderComponent } from './layout/app-header/app-header.component';
 import { AppFooterComponent } from './layout/app-footer/app-footer.component';
 import { IndexComponent } from './modules/index/index.component';
-import { MatSliderModule } from '@angular/material/slider'
+import { MatSliderModule } from '@angular/material/slider';
+import { HeaderComponent } from './layout/header/header.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list'
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,12 +23,19 @@ import { MatSliderModule } from '@angular/material/slider'
     AppHeaderComponent,
     AppFooterComponent,
     IndexComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
