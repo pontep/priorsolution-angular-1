d9b3b2aad8c5e0c2cdff3c9c725527067d1913f8
pontep <pontep.taveeshup@gmail.com>
2020-11-12 10:19:48 +0700

    add angular material

--- din-app/src/app/app-routing.module.ts
+++ din-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './modules/index/index.component';
+import { Page2Component } from './modules/page2/page2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent
+  },
+  {
+    path: 'page2',
+    component: Page2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- din-app/src/app/app.module.ts
+++ din-app/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { Page2Component } from './modules/page2/page2.component';
+import { AppHeaderComponent } from './layout/app-header/app-header.component';
+import { AppFooterComponent } from './layout/app-footer/app-footer.component';
+import { IndexComponent } from './modules/index/index.component';
+import { MatSliderModule } from '@angular/material/slider'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page2Component,
+    AppHeaderComponent,
+    AppFooterComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
