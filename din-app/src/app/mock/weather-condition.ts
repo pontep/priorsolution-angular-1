2de71d0c1711acd627be23f8593eeffa52356976
pontep <pontep.taveeshup@gmail.com>
2020-11-17 12:55:49 +0700

    decorating

---
+++ din-app/src/app/mock/weather-condition.ts
@@ -0,0 +1,31 @@
+export const WEATHER_CONDITION: string[] = [
+  'ความหมาย',
+  'ท้องฟ้าแจ่มใส (Clear)',
+  'มีเมฆบางส่วน (Partly cloudy)',
+  'เมฆเป็นส่วนมาก (Cloudy)',
+  'มีเมฆมาก (Overcast)',
+  'ฝนตกเล็กน้อย (Light rain)',
+  'ฝนปานกลาง (Moderate rain)',
+  'ฝนตกหนัก (Heavy rain)',
+  'ฝนฟ้าคะนอง (Thunderstorm)',
+  'อากาศหนาวจัด (Very cold)',
+  'อากาศหนาว (Cold)',
+  'อากาศเย็น (Cool)',
+  'อากาศร้อนจัด (Very hot)',
+];
+
+export const WEATHER_CONDITION_ICON: string[] = [
+    'รูปภาพ',
+    'weather-sunny',
+    'weather-partly-cloudy',
+    'weather-cloudy',
+    'weather-cloudy-alert',
+    'weather-partly-rainy',
+    'weather-rainy',
+    'weather-pouring',
+    'weather-lightning-rainy',
+    'weather-snowy-heavy',
+    'weather-snowy',
+    'snowflake',
+    'thermometer-alert'
+]
--- din-app/src/app/modules/index/index.component.ts
+++ din-app/src/app/modules/index/index.component.ts
@@ -1,61 +1,68 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
 import { WeatherForecast } from 'src/app/models/weatherForecast';
 import { ForecastService } from 'src/app/services/forecast.service';
-import { THAI_PROVINCES } from "../../source/thai-province";
-import { FORECAST } from "../../models/mock-forecast";
+import { THAI_PROVINCES } from '../../source/thai-province';
+import { FORECAST } from '../../models/mock-forecast';
+import {
+  WEATHER_CONDITION,
+  WEATHER_CONDITION_ICON,
+} from '../../mock/weather-condition';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
-
-  loading: boolean
+  loading: boolean;
   myControl = new FormControl();
-  options = THAI_PROVINCES
+  options = THAI_PROVINCES;
   filteredOptions: Observable<string[]>;
-  selectedProvince: string
-  forecast: WeatherForecast
+  selectedProvince: string;
+  forecast: WeatherForecast;
+  weatherCondition = WEATHER_CONDITION;
+  weatherConditionIcon = WEATHER_CONDITION_ICON;
+  @Input() title: string;
 
-  @Input() title: string
-  
-  constructor(private forecastService: ForecastService) { }
+  constructor(private forecastService: ForecastService) {}
 
-  getForecast(): void{
-    this.loading = true
-    this.forecastService.getForecast(this.selectedProvince).subscribe(forecast => {
-      this.forecast = forecast
-      this.loading = false
-      console.log(forecast);
-    })
-      
+  getForecast(): void {
+    this.loading = true;
+    this.forecastService
+      .getForecast(this.selectedProvince)
+      .subscribe((forecast) => {
+        this.forecast = forecast;
+        this.loading = false;
+        console.log(forecast);
+      });
   }
 
-  onSubmit(data): void{
+  onSubmit(data): void {
     console.log(data);
-    
   }
   ngOnInit(): void {
-    this.getTitle()
+    console.log(this.weatherCondition);
+    
+    console.log(this.weatherConditionIcon);
+    
+    this.getTitle();
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
-      map(value => this._filter(value))
+      map((value) => this._filter(value))
     );
     // this.forecast = FORECAST
-    
   }
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
-    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
-  }
-  
-  getTitle(): void{
-    this.title = 'din-app'
+    return this.options.filter(
+      (option) => option.toLowerCase().indexOf(filterValue) === 0
+    );
   }
 
-
+  getTitle(): void {
+    this.title = 'din-app';
+  }
 }
