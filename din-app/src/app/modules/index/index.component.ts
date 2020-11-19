20f942be41b84157aa4c85ca266bfc30c8f8feba
pontep <pontep.taveeshup@gmail.com>
2020-11-19 18:41:50 +0700

    doing loginForm validation

--- din-app/src/app/modules/index/index.component.ts
+++ din-app/src/app/modules/index/index.component.ts
@@ -1,71 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
-import { WeatherForecast } from 'src/app/models/weatherForecast';
-import { ForecastService } from 'src/app/services/forecast.service';
-import { THAI_PROVINCES } from '../../source/thai-province';
-import {
-  WEATHER_CONDITION,
-  WEATHER_CONDITION_ICON,
-} from '../../mock/weather-condition';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
-  loading: boolean;
-  myControl = new FormControl();
-  options = THAI_PROVINCES;
-  filteredOptions: Observable<string[]>;
-  selectedProvince: string;
-  forecast: WeatherForecast;
-  weatherCondition = WEATHER_CONDITION;
-  weatherConditionIcon = WEATHER_CONDITION_ICON;
-  user: string = null
+  loginForm: FormGroup
 
-
-
-  @Input() title: string;
-
-  constructor(private forecastService: ForecastService) {}
-
-  getForecast(): void {
-    this.loading = true;
-    this.forecastService
-      .getForecast(this.selectedProvince)
-      .subscribe((forecast) => {
-        this.forecast = forecast;
-        this.loading = false;
-        console.log(forecast);
-      });
+  constructor() {}
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+  login() {
+    console.log(this.email.value, this.password.value);
   }
 
-  onSubmit(data): void {
-    console.log(data);
+  get email() {
+    return this.loginForm.get('email');
   }
-  ngOnInit(): void {
-    console.log(this.weatherCondition);
-    
-    console.log(this.weatherConditionIcon);
-    
-    this.getTitle();
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map((value) => this._filter(value))
-    );
-    // this.forecast = FORECAST
-  }
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(
-      (option) => option.toLowerCase().indexOf(filterValue) === 0
-    );
-  }
-
-  getTitle(): void {
-    this.title = 'din-app';
+  get password() {
+    return this.loginForm.get('password');
   }
 }
