c04731212a7cbb09b068937c5639e5a9ee44a7ad
pontep <pontep.taveeshup@gmail.com>
2020-11-20 15:22:32 +0700

    add npm i @rxweb/reactive-form-validators

--- din-app/src/app/modules/example/example.component.ts
+++ din-app/src/app/modules/example/example.component.ts
@@ -1,39 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { RxwebValidators } from '@rxweb/reactive-form-validators';
 
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
-  styleUrls: ['./example.component.css']
+  styleUrls: ['./example.component.css'],
 })
 export class ExampleComponent implements OnInit {
+  loading: boolean;
+  exampleValidateForm: FormGroup;
 
-  loading: boolean
-  exampleValidateForm: FormGroup
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.exampleValidateForm = new FormGroup({
-      month: new FormControl('',[
-        Validators.required,
-        Validators.min(1),
-        Validators.max(11)
-      ])
-    })
+      value1: new FormControl('', [Validators.required]),
+      value2: new FormControl('', [Validators.required]),
+      compareType: new FormControl('', [Validators.required]),
+    });
   }
-  onSubmit(){
-    this.loading = true
-    setTimeout(()=>{
-      alert(`Month input is ${this.month.value}`)
-      this.loading = false
-    }, 500)
-    
-  }
-  
-  get month(){
-    return this.exampleValidateForm.get('month')
+  onSubmit() {
+    this.loading = true;
+    setTimeout(() => {
+      alert(`Month input is ${this.exampleValidateForm.status}`);
+      this.loading = false;
+    }, 500);
   }
 
+  handleCompare() {
+    switch (this.compareType.value) {
+      case 'Greater than':
+        console.log('Greater than');
+        this.exampleValidateForm.get("value1").setValidators([RxwebValidators])
+        break;
+      case 'Less than':
+        console.log('Less than');
+        break;
+      case 'Equal':
+        console.log('Equal');
+        break;
+      default:
+        console.log('Default case');
+        break;
+    }
+  }
 
+  get value1() {
+    return this.exampleValidateForm.get('value1');
+  }
+  get value2() {
+    return this.exampleValidateForm.get('value2');
+  }
+  get compareType() {
+    return this.exampleValidateForm.get('compareType');
+  }
 }
